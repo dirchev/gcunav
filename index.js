741769c5d8a2c698ec5ec3c3719982dbbe534d68
Dimitar Mirchev <dimitar.mirchev96@gmail.com>
2017-03-11 23:13:11 +0000

    support env IP and PORT

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var express = require('express')
 var app = express()
 
 require('./lib/loadRoutes')(app, path.join(process.cwd(), '/server/http'), function () {
-  app.listen(3000, function () {
+  var port = process.env.PORT || 3000
+  var ip = process.env.IP || '127.0.0.1'
+  app.listen(port, ip, function () {
     console.log('Example app listening on port 3000!')
   })
 })

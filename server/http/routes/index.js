bf26f9f8a2421e16bcb18895a0d098aff6ff9181
dirchev <dirchev@camplight.net>
2017-03-12 16:40:41 +0200

    move home to index

--- server/http/routes/index.js
+++ server/http/routes/index.js
@@ -2,12 +2,23 @@ module.exports = function (helpers) {
   return {
     'GET /': [
       function (req, res, next) {
-        res.body = `
-          <div>
-            <h1>That works</h1>
-          </div>
-          <a href="/home">Home</a>
-        `
+        res.view = 'home'
+        res.viewData = {
+          events: [
+            {
+              id: 1,
+              name: 'Graduation Fair'
+            },
+            {
+              id: 2,
+              name: 'Gaming Fair'
+            },
+            {
+              id: 3,
+              name: 'Some Other Fair'
+            }
+          ]
+        }
         next()
       }
     ]

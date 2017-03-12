54a3d617e72a9285aa0d40190348c958d6cc0db4
dirchev <dirchev@camplight.net>
2017-03-12 16:39:17 +0200

    add support for view and viewData

--- server/http/responder.js
+++ server/http/responder.js
@@ -3,6 +3,8 @@ module.exports = function (app) {
   app.use(function (req, res, next) {
     if (res.body) {
       res.send(res.body)
+    } else if (res.view) {
+      res.render(res.view, res.viewData)
     } else {
       res.status(404).end()
     }

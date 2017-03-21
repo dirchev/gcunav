9a83db1fe8888ebe22fd2273ed7cdbdf50e5bba6
dirchev <dirchev@camplight.net>
2017-03-21 12:40:11 +0000

    update search place API to use the new room model mehtod

--- server/http/routes/search-place.js
+++ server/http/routes/search-place.js
@@ -1,18 +1,23 @@
-var Place = require('../../models/place.js')
+var Room = require('../../models/room.js')
 
 module.exports = function (helpers) {
+  var getRooms = function (req, res, next) {
+    // get the text parameter
+    var text = req.query.text
+
+    // get places by search text
+    Room.search(text, function (err, results) {
+      if (err) return next(err)
+      req.rooms = results
+      next()
+    })
+  }
   return {
     'GET /': [
+      getRooms,
       function (req, res, next) {
-        // get the text parameter
-        var text = req.query.text || ''
-
-        // get places by search text
-        var places = Place.search(text)
-
-        // choose the view and pass data
         res.view = 'search-place'
-        res.viewData = {places: places, searchText: text}
+        res.viewData = {rooms: req.rooms, searchText: req.query.text || ''}
 
         // go to the next handler
         next()

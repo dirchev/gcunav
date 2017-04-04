77ee434af0f069837d6a2055eb9b62abb7b1b725
dirchev <dirchev@camplight.net>
2017-04-04 14:00:46 +0100

    update responder to set logged user to view data

--- server/http/responder.js
+++ server/http/responder.js
@@ -1,4 +1,24 @@
+var $require = require(process.cwd() + '/lib/require')
+var User = $require('server/models/user')
+
 module.exports = function (app) {
+  // setting logged user data
+  app.use(function (req, res, next) {
+    var userCookie = req.cookies.user
+    if (userCookie) {
+      User.getByCookieHash(userCookie, function (err, users) {
+        if (err) return next(err)
+        if (users.length) {
+          res.user = users[0]
+          res.viewData = res.viewData || {}
+          res.viewData.loggedUser = res.user
+        }
+        return next()
+      })
+    } else {
+      return next()
+    }
+  })
   // default not found handler
   app.use(function (req, res, next) {
     if (res.body) {

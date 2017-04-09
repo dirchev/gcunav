5584cae20face2114c561a1aa817321620a9a2a6
dirchev <dirchev@camplight.net>
2017-04-09 15:25:10 +0100

    push featured buildings and rooms to home page front-end

--- server/http/routes/index.js
+++ server/http/routes/index.js
@@ -1,7 +1,32 @@
+var $require = require(process.cwd() + '/lib/require')
+var Room = $require('server/models/room')
+var Building = $require('server/models/building')
+
 module.exports = function (helpers) {
+  var getFeaturedRooms = function (req, res, next) {
+    Room.getFeatured(function (err, rooms) {
+      if (err) return next(err)
+      req.featuredRooms = rooms
+      next()
+    })
+  }
+
+  var getFeaturedBuildings = function (req, res, next) {
+    Building.getFeatured(function (err, buildings) {
+      if (err) return next(err)
+      req.featuredBuildings = buildings
+      next()
+    })
+  }
   return {
     'GET /': [
+      getFeaturedRooms,
+      getFeaturedBuildings,
       function (req, res, next) {
+        res.viewData = {
+          featuredRooms: req.featuredRooms,
+          featuredBuildings: req.featuredBuildings,
+        }
         res.view = 'home'
         next()
       }
